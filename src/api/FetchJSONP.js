fc202951f003a29502b71b7bc76c5f6579b9ef7e
Arianrod <tatyana.comolyh@yandex.ru>
2022-01-13 12:18:45 +0300

    Добавлен build проекта

--- src/api/FetchJSONP.js
+++ src/api/FetchJSONP.js
@@ -1,40 +1,47 @@
-import fetchJsonp from 'fetch-jsonp'
-import { receiveTokken } from '../api/ReceiveTokken.js'
+import fetchJsonp from "fetch-jsonp";
+import { receiveTokken } from "../api/ReceiveTokken.js";
 
 export default async function selectAPI(option) {
+  //console.log(receiveTokken())
 
-   //console.log(receiveTokken())
+  const TOKEN =
+    "76dd833da3b5e62c58e8830dfabc225cc5b898268115a8baaac76ea0b77d28831bd968bc35d694c105b07";
 
-   const TOKEN = '76dd833da3b5e62c58e8830dfabc225cc5b898268115a8baaac76ea0b77d28831bd968bc35d694c105b07'
+  //https://oauth.vk.com/authorize?client_id=8018671&display=page&redirect_uri=&scope=Database&response_type=token&v=5.52
 
-   //https://oauth.vk.com/authorize?client_id=8018671&display=page&redirect_uri=&scope=Database&response_type=token&v=5.52
-
-   if(option.method == 'database.getUniversities') {
-      let res = await fetchJsonp(`https://api.vk.com/method/${option.method}?count=${option.count}&city_id=${option.city_id}&country_id=${option.country_id}&q=${option.q}&access_token=${TOKEN}&v=5.131`)
-      .then(function(response) {
-         return response.json()
-      }).then(function(json) {
-         console.log('parsed json', json)
-         return json.response.items
-      }).catch(function(ex) {
-         console.log('parsing failed', ex)
+  if (option.method == "database.getUniversities") {
+    let res = await fetchJsonp(
+      `https://api.vk.com/method/${option.method}?count=${option.count}&city_id=${option.city_id}&country_id=${option.country_id}&q=${option.q}&access_token=${TOKEN}&v=5.131`
+    )
+      .then(function (response) {
+        return response.json();
       })
-
-      return res
-   }
-   else if (option.method == 'database.getCities') {
-      let res = await fetchJsonp(`https://api.vk.com/method/${option.method}?count=${option.count}&country_id=${option.country_id}&region_id=&q=${option.q}&access_token=${TOKEN}&v=5.131`)
-      .then(function(response) {
-         return response.json()
-      }).then(function(json) {
-         //console.log('parsed json', json)
-         return json.response.items
-      }).catch(function(ex) {
-         console.log('parsing failed', ex)
+      .then(function (json) {
+        console.log("parsed json", json);
+        return json.response.items;
       })
+      .catch(function (ex) {
+        console.log("parsing failed", ex);
+      });
 
-      return res
-   }
+    return res;
+  } else if (option.method == "database.getCities") {
+    let res = await fetchJsonp(
+      `https://api.vk.com/method/${option.method}?count=${option.count}&country_id=${option.country_id}&region_id=&q=${option.q}&access_token=${TOKEN}&v=5.131`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        //console.log('parsed json', json)
+        return json.response.items;
+      })
+      .catch(function (ex) {
+        console.log("parsing failed", ex);
+      });
+
+    return res;
+  }
 }
 
-export {selectAPI} 
+export { selectAPI };
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,30 @@
-import Vue from 'vue'
-import BootstrapVue from "bootstrap-vue"
-import VueRouter from 'vue-router'
-import "bootstrap/dist/css/bootstrap.min.css"
-import 'bootstrap-vue/dist/bootstrap-vue.css'
- 
-Vue.use(VueRouter)
-Vue.use(BootstrapVue)
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import VueRouter from "vue-router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
 
 const routes = [
   {
-      path: '/',
-      name: 'SummaryUser',
-      component: () => import(/* webpackChunkName: "about" */ '../views/SummaryUser.vue')
+    path: "/",
+    name: "SummaryUser",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/SummaryUser.vue"),
   },
   {
-      path: '/unitconversion',
-      name: 'UnitConversion',
-      component: () => import(/* webpackChunkName: "about" */ '../views/UnitConversion.vue')
-  }
-]
+    path: "/unitconversion",
+    name: "UnitConversion",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/UnitConversion.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
